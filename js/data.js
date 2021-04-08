ae2aea00246643490b902c9583aa230b35791c20
Synapsis CI <webmaster@synapsis.com.ar>
2021-04-08 01:11:56 -0300

    second

---
+++ js/data.js
@@ -0,0 +1,29 @@
+var data = {
+  name : 'Anthony Mas y rubi',
+  bio: [
+    {name: 'Nombre', value: 'Anthony Mas y rubi'},
+    {name: 'Email', value: 'anthoni97@gmail.com'},
+    {name: 'Teléfono', value: '+541159056478'},
+    {name: 'País', value: 'Argentina'},
+    {name: 'Ciudad', value: 'Buenos Aires'}
+  ],
+  github: 'AJVzla',
+  projects: [
+    {name: 'ITM', url: 'https://ortopediaitm.com.ar/'},
+    {name: 'Metis', url: 'https://www.metis.com.ar/'},
+    {name: 'Golden Shield Capital', url: 'https://goldenshieldcapital.com/'},
+    {name: 'LionGrove', url: 'https://liongrove.com/'},
+    {name: 'Brickants', url: 'https://brickants.com/es/'},
+    {name: 'Torre Sakura', url: 'https://torresakura.com/es/'},
+    {name: 'La Constancia', url: 'http://laconstancia.synapsisci.servidoraweb.net/'},
+    {name: 'Produk Bons', url: 'https://ajvzla.github.io/tbons/'},
+    {name: 'Tresseiscero', url: 'https://tresseiscero.com.ar/'}
+  ],
+  resume: 'https://ajvzla.files.wordpress.com/2021/04/anthony_masyrubi.pdf',
+  socials: [
+    {name: 'LinkedIn', url: 'https://www.linkedin.com/in/anthony-mas-y-rubi/'},
+    {name: 'GitHub', url: 'https://github.com/AJVzla'},
+    {name: 'Instagram', url: 'https://www.instagram.com/ajguns/'},
+    // {name: 'Website', url: 'https://ahsankhan.me'}
+  ]
+}
---
+++ js/script.js
@@ -0,0 +1,115 @@
+let input = document.querySelector("input");
+let terminalBody = document.querySelector("#terminalBody");
+
+let commandList = [ 'bio 🧔', 'cv 📄', 'proyectos 💻',  'rrss 📱',  'borrar ❌',];
+
+functionCalls();
+
+function functionCalls() {
+    checkWindowClick();
+    checkPressedEnter();
+}
+
+function checkWindowClick() {
+    terminalBody.addEventListener ('click', function(event){
+        // console.log("clicked!");
+        input.focus();
+    });
+}
+
+function checkPressedEnter() {
+    input.addEventListener('keydown', function(e){
+        // console.log("function");
+        if (e.key === 'Enter') {
+            // console.log("Enter");
+            execute(input.value);
+        }
+    });
+}
+
+function execute(inputValue) {
+    let temp = input.value;
+    input.remove();
+    terminalBody.innerHTML += temp;
+    checkCommand(temp);
+    addInput();
+}
+
+function checkCommand(inputCommand){
+
+    let command = inputCommand.split(" ")[0];
+
+    if(command){
+       
+        if(command === 'bio'){ commandBio();}
+        else if(command === 'borrar'){ commandClear();}
+        else if(command === 'help'){  terminalBody.innerHTML +=  '<p class="msj">Escribe alguno de estos comandos: 🡫<p/>'; commandHelp(); }      
+        else if(command === 'proyectos'){ commandProject();}
+        else if(command === 'cv'){ commandResume();}
+        else if(command === 'rrss'){ commandSocial();}
+        else{
+            terminalBody.innerHTML +=  '<br>' + inputCommand + ' is not recognized as a command, Try \"help\"<br>';
+        }
+    }
+    else {
+        terminalBody.innerHTML += '<br>';
+    }
+}
+
+function addInput() {
+    terminalBody.innerHTML += ' > <input type="text" autofocus />';
+    input = document.querySelector("input");
+    input.focus();
+    functionCalls();
+}
+
+function commandBio() {
+
+    terminalBody.innerHTML += '<br><br>';
+    for(let i=0; i<data.bio.length; i++) {
+        terminalBody.innerHTML += `${data.bio[i].name}: ` ;
+        terminalBody.innerHTML += `${data.bio[i].value}<br>` ;
+    }
+    terminalBody.innerHTML += '<br>';
+}
+
+
+
+function commandClear() {
+    terminalBody.innerHTML = ``;
+}
+
+function commandHelp() {
+    terminalBody.innerHTML += '';
+    for(let i=0; i<commandList.length; i++) {
+        terminalBody.innerHTML += `${commandList[i]}<br>`
+    }
+}
+
+
+
+function commandSocial() {
+
+    terminalBody.innerHTML += '<br><br>';
+    for(let i=0; i<data.socials.length; i++) {
+        terminalBody.innerHTML += `${data.socials[i].name}: ` ;
+        terminalBody.innerHTML += `<a href=${data.socials[i].url} target="_blank">${data.socials[i].url}</a><br>` ;
+    }
+    terminalBody.innerHTML += '<br>';
+}
+
+function commandProject() {
+
+    terminalBody.innerHTML += '<br><br>';
+    for(let i=0; i<data.projects.length; i++) {
+        terminalBody.innerHTML += `${data.projects[i].name}: ` ;
+        terminalBody.innerHTML += `<a href="${data.projects[i].url}" target="_blank">${data.projects[i].url}</a><br>` ;
+    }
+    terminalBody.innerHTML += '<br>';
+}
+
+function commandResume() {
+    terminalBody.innerHTML += `<br><a href=${data.resume}>Descargar PDF</a><br>`;
+}
+
+
